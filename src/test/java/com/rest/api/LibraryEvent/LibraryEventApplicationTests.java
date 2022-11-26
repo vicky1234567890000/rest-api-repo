3990035d66f491f91b2e7d363b2d0bc92237b082
Abhisek Das <40667508+vicky1234567890000@users.noreply.github.com>
2022-11-26 17:49:47 +0530

    new changes

---
+++ src/test/java/com/rest/api/LibraryEvent/LibraryEventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rest.api.LibraryEvent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryEventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
